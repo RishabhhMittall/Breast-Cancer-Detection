c90a66b2ad5d862271a055e0363951195fd62091
Rishabh Mittal <rishabhmittal2407@gmail.com>
2025-04-13 19:52:54 +0530

    name changes

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ CORS(app)
 # Define expected feature names
 FEATURE_NAMES = [
     "radius_mean", "texture_mean", "perimeter_mean", "area_mean", "smoothness_mean",
-    "compactness_mean", "concavity_mean", "concave points_mean", "symmetry_mean", "fractal_dimension_mean",
+    "compactness_mean", "concavity_mean", "concave_points_mean", "symmetry_mean", "fractal_dimension_mean",
     "radius_se", "texture_se", "perimeter_se", "area_se", "smoothness_se",
     "compactness_se", "concavity_se", "concave points_se", "symmetry_se", "fractal_dimension_se",
     "radius_worst", "texture_worst", "perimeter_worst", "area_worst", "smoothness_worst",
